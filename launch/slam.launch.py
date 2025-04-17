a3edb094569e4f84e8748922ff7e56430e38b530
Tanjim-5125 <shadman0954.au@gmail.com>
2025-04-17 16:07:02 +1000

    Create slam.launch.py

---
+++ launch/slam.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    slam_params_file = LaunchConfiguration('slam_params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'slam_params_file',
+            default_value=os.path.join(
+                get_package_share_directory("par_1"), 'config', 'slam.yaml'
+            ),
+            description='Full path to the slam_toolbox config file',
+        ),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation time',
+        ),
+
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[slam_params_file, {'use_sim_time': use_sim_time}],
+        )
+    ])
